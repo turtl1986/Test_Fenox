8073d3f16fa18705de2a90c8063d788bbe7437e1
Дарья Якимович <yakimovichdari@gmail.com>
2024-05-03 13:06:15 +0300

    add:add style bonus

--- src/components/Bonus/Bonus.tsx
+++ src/components/Bonus/Bonus.tsx
@@ -1,16 +1,24 @@
 import style from './bonus.module.css'
+import bonus from '../../assets/surp.svg'
 
 export function Bonus(){
     return(
         <div className={style.container}>
             <div className={style.bonus}>
-                
-        <h2>Дарим бонус 
+               <div className={style.description}>
+               <h2 className={style.title}>Дарим бонус 
 за регистрацию: 100 баллов </h2>
-<div>
-    <button type="button">Войти</button>
-    <button type="button">Зарегистрироваться</button>
+<div className={style.setting}>
+<button className={style.button_white} type="button">Войти</button>
+    <button className={style.button} type="button">Зарегистрироваться</button>
+
 </div>
+               </div>
+                
+<img className={style.image} src={bonus} alt="bonus" />
+               
+                
+      
             </div>
         </div>
     )
